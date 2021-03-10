bd9644587198c532c3deb19a9e2b4d9e3be7d3bb
Gorans-989 <gorans989@yahoo.com>
2021-03-10 18:54:19 +0100

    0

--- JS_Advance/Homeworks/Class11_classes_part_1/script.js
+++ JS_Advance/Homeworks/Class11_classes_part_1/script.js
@@ -29,9 +29,63 @@ class Animal {
         }
     }
 }
+
 let typeOfAnimal = ["carnivore", "herbivore", "omnivore"];
 let dog = new Animal ("sputnik", 12, typeOfAnimal[1], 120);
 let elephant = new Animal ('dambo', 6, typeOfAnimal[1], 500);
 let crocodile = new Animal ('koko', 7, typeOfAnimal[0], 250);
 let tiger = new Animal ('misko', 9, typeOfAnimal[2], 300);
 let hippo = new Animal ('bobo', 14, typeOfAnimal[1], 420);
+
+
+class Person {
+    constructor (firstName, lastName, age, address) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.address = address;
+    }
+
+    fullName = () => {
+        return console.log(`${this.firstName} ${this.lastName}`);
+    }
+    // get something () {
+    //     // log a message
+    //     console.log(`(get) The result is:_________`)
+    // }
+
+    // set  firstName (firstName) {
+    //     if(!firstName) {
+    //         console.log(`Please input a valid name`);
+    //     }
+    //     if(firstName) {
+    //         console.log(`we are setting the name`);
+    //     }
+    //     //do validation
+    //     console.log(`Please wait for the result.`)
+    // }
+}
+
+class Student extends Person {
+    constructor(firstName, lastName, age, address,subjects, academy) {
+        super(firstName, lastName, age, address);
+        this.academy = academy;
+        this.subjects = subjects;
+    }
+    static studentMethod (student, subject) {
+        if(student instanceof Student) {
+            if (student.subjects.includes(subject)) {
+                console.log(`The student ${student.firstName} ${student.lastName} is already studying ${subject}`);
+            }
+            else {
+                console.log(`The student ${student.firsName} ${student.lastName} doesnt study ${subject}`);
+            }
+        }
+        else { throw new Error ("The first parameter is not a student")}
+    }
+}
+
+let student1 = new Student('ivan', 'ivanov', 23, 'partizanska', ['math', 'science', 'biology'], 'semos');
+let student2 = new Student('iva', 'petrova', 31, 'razloska', ["history", "english", "science"], 'different');
+
+Student.studentMethod(student1, "science")
